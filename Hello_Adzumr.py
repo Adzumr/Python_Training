8634efb97295b1e2f53d945b618ba15bce37ed47
Adzumr Jada <adzumrjada@gmail.com>
2017-07-21 17:30:10 +0100

    print_Hello

---
+++ Hello_Adzumr.py
@@ -0,0 +1,27 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:53:40) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num = 5
+>>> if num < 10:
+	print (num, 'Is Less Than 10')
+
+	
+(5, 'Is Less Than 10')
+>>> numb = input
+>>> if numb < 10:
+	print 'Access Denied'
+
+	
+>>> 
+>>> 
+
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> number = input
+>>> if number < 15:
+	print (number, 'Is not allowed')
+
+	
+>>> 
